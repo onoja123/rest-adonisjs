2c2ff7de9f91d5fcf9be6f4233e3cb29f7b385dc
okpe onoja <okpeonoja18@gmail.com>
2023-02-14 02:10:13 +0100

    first push

---
+++ Downloads/perso/src/utils/data.js
@@ -0,0 +1,155 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Colabration",
+    projects: 76,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Signs",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  // {
+  //   name: "Brand Identity",
+  //   projects: 47,
+  //   icon: MdWorkspacesOutline,
+  //   bg: "#F26440",
+  // },
+];
+
+export const WhatDoIHelp = [
+"We are a new record label who are ready to show you new young talented artists"
+];
+
+export const workExp = [
+  {
+    place: "Blaqrose",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Musician",
+    detail:
+      "A very talend artists ",
+  },
+  {
+    place: "Wale",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "CEO",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  // {
+  //   place: "Global Solution",
+  //   tenure: "Aug 2014 - Sep 2016",
+  //   role: "Sr. Product Designer",
+  //   detail:
+  //     "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  // },
+];
+
+export const comments = [
+  {
+    name: "Akintade Seun",
+    post: "Software developer",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Allen ",
+    post: "Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Tush Wale",
+    post: "CEO",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Okpe Onoja",
+    post: "Software developer",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Akintade Seun",
+    post: "Software developer",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Allen ",
+    post: "Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Tush Wale",
+    post: "CEO",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Okpe Onoja",
+    post: "Software developer",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },  {
+    name: "Tush Wale",
+    post: "CEO",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  }
+
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
